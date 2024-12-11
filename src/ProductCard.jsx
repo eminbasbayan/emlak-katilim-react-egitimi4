fe7f225f9fab40fb1ae1305f2b052d185bc79b34
eminbasbayan <emin.basbayan@icloud.com>
2024-12-11 13:51:52 +0300

    product card css

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,21 +1,18 @@
-const productImageCss = {
-  width: '200px',
-};
+import './ProductCard.css';
 
 function ProductCard() {
   return (
-    <div className="product-card" style={{
-        backgroundColor: "red"
-    }}>
+    <div className="product-card">
       <div className="product-image">
         <img
           src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
           alt="product image"
-          style={productImageCss}
         />
       </div>
-      <b className="product-title">Product Title</b>
-      <span className="product-price">100₺</span>
+      <div className="product-bottom">
+        <b className="product-title">Product Title</b>
+        <span className="product-price">100₺</span>
+      </div>
     </div>
   );
 }
