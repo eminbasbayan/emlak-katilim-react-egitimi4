2d2431ee92f0bcab05de5b3fe54ba619073d44be
eminbasbayan <emin.basbayan@icloud.com>
2024-12-11 15:40:33 +0300

    map ve keys kullanımı

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,28 +1,23 @@
 import ProductCard from './ProductCard';
 import { productsData } from './productsData';
 
-import "./Products.css";
+import './Products.css';
 
 function Products() {
   return (
     <div className="products">
       <h1>Products</h1>
       <div className="products-wrapper">
-        <ProductCard
-          image={productsData[0].image}
-          title={productsData[0].title}
-          price={productsData[0].price}
-        />
-        <ProductCard
-          image={productsData[1].image}
-          title={productsData[1].title}
-          price={productsData[1].price}
-        />
-        <ProductCard
-          image={productsData[2].image}
-          title={productsData[2].title}
-          price={productsData[2].price}
-        />
+        {productsData.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+            />
+          );
+        })}
       </div>
     </div>
   );
