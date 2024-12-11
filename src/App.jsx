dd36252b77b0654cdb35c0da93b4414c19fdbf0c
eminbasbayan <emin.basbayan@icloud.com>
2024-12-11 10:43:37 +0300

    component empty tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App(){
-    return <h1>Hello, React!</h1>
+function App() {
+  return (
+    <>
+      <h1>App Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, aut
+        ad dolorum nesciunt, obcaecati nam alias dolores nisi ab, quia
+        reprehenderit magnam quis quidem vel nemo exercitationem corrupti
+        dignissimos odit?
+      </p>
+    </>
+  );
 }
 
-export default App
+export default App;
