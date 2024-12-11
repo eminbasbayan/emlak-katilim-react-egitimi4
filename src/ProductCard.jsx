1ce0caf85c8b7e388ce7bdc48258a260ef6f8ac1
eminbasbayan <emin.basbayan@icloud.com>
2024-12-11 12:23:58 +0300

    component inline css

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,8 +1,18 @@
+const productImageCss = {
+  width: '200px',
+};
+
 function ProductCard() {
   return (
-    <div className="product-card">
+    <div className="product-card" style={{
+        backgroundColor: "red"
+    }}>
       <div className="product-image">
-        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="product image" />
+        <img
+          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          alt="product image"
+          style={productImageCss}
+        />
       </div>
       <b className="product-title">Product Title</b>
       <span className="product-price">100₺</span>
@@ -10,4 +20,4 @@ function ProductCard() {
   );
 }
 
-export default ProductCard
+export default ProductCard;
