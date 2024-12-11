9fe140b855d9e143cb4066b5c293734eb16d2183
eminbasbayan <emin.basbayan@icloud.com>
2024-12-11 15:10:26 +0300

    componentlerde javascript dosyası kullanımı

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,14 +1,15 @@
 import ProductCard from './ProductCard';
+import { productsData } from './productsData';
 
 function Products() {
-  const image = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg';
-  const title = 'Product Title';
-  const price = 100;
-
   return (
     <div className="products">
       <h1>Products</h1>
-      <ProductCard image={image} title={title} price={price} />
+      <ProductCard
+        image={productsData[0].image}
+        title={productsData[0].title}
+        price={productsData[0].price}
+      />
     </div>
   );
 }
