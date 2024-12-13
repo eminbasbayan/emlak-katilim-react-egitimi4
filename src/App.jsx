4aab39f8fa3fc7d4d6dcb4dc8641836fc057a08c
eminbasbayan <emin.basbayan@icloud.com>
2024-12-13 14:36:37 +0300

    ilk route oluşturma

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import HomePage from "./pages/HomePage";
-import ProductsPage from "./pages/ProductsPage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <div className="app container mx-auto">
-      <section>
-  {/*      <HomePage /> */}
-       <ProductsPage />        
-       <AboutPage />        
-       <ContactPage />        
-      </section>
+      <RouterProvider router={router} />
     </div>
   );
 }
