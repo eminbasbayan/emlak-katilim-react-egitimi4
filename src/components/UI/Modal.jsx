0c86e05b458473af11500bbe1f75bf55424352ca
eminbasbayan <emin.basbayan@icloud.com>
2024-12-13 10:09:02 +0300

    click out side örneği

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -10,8 +10,8 @@ const Modal = ({
   }
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-      <div className="relative p-4 w-full max-w-2xl max-h-full m-auto">
+    <div className="modal overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div className="relative p-4 w-full max-w-2xl max-h-full m-auto z-50">
         {/* Modal content  */}
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
           {/*  Modal header */}
@@ -68,6 +68,7 @@ const Modal = ({
           </div>
         </div>
       </div>
+      <div className='empty-div w-full h-full bg-slate-400 absolute top-0 left-0 opacity-50' onClick={handleCloseModal}></div>
     </div>
   );
 };
