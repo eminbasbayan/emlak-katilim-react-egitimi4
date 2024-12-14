7b7f04ebda232e83ccd4f27c81e0d79165232361
eminbasbayan <emin.basbayan@icloud.com>
2024-12-14 10:34:33 +0300

    react redux kurulumu

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 import CartProvider from './context/CartProvider';
@@ -7,9 +9,11 @@ import './index.css';
 import ThemeProvider from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <CartProvider>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </CartProvider>
+  <ReduxProvider store={store}>
+    <CartProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </CartProvider>
+  </ReduxProvider>
 );
